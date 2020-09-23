e51833382b18e8458d1c839d34cce21ac738c94e
Matt Pasco <mapasco@microsoft.com>
2020-09-23 09:33:05 -0600

    adding init

---
+++ src/VDWM.App/MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Interop;
+using System.Runtime.InteropServices;
+
+using VDWM.Core;
+using System.Printing;
+using System.ComponentModel;
+
+namespace VDWM.App
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        [DllImport("user32.dll", EntryPoint = "RegisterHotKey")]
+        public static extern bool RegisterHotKey(IntPtr hWnd, int id, int fsModifiers, int vlc);
+        [DllImport("user32.dll")]
+        public static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+        private DesktopManager dwm;
+
+        private const int HOT_KEY = 6; // control + shift
+        private IntPtr windowHndl;
+
+        private int[] keys = { 0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39 };
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            dwm = new DesktopManager();
+            
+        }
+
+        private void Grid_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            windowHndl = new WindowInteropHelper(this).Handle;
+            for (int i = 0; i < keys.Length; i++)
+            {
+                RegisterHotKey(windowHndl, i, HOT_KEY, keys[i]);
+            }
+            var source = HwndSource.FromHwnd(windowHndl);
+            source.AddHook(WndProc);
+        }
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            for (int i = 0; i < keys.Length; i++)
+            {
+                UnregisterHotKey(windowHndl, i);
+            }
+            base.OnClosing(e);
+        }
+
+        private IntPtr WndProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            // Handle messages...
+            if (msg == 0x0312)
+            {
+                int vdId = (int)wParam;
+                dwm.ChangeToVD(vdId);
+                handled = true;
+            }
+
+            return IntPtr.Zero;
+        }
+
+
+    }
+}
---
+++ src/VDWM.Core/DesktopManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using WindowsDesktop;
+
+namespace VDWM.Core
+{
+    public class DesktopManager
+    {
+        private ICollection<VirtualDesktop> desktops
+        {
+            get => VirtualDesktop.GetDesktops();
+        }
+
+        private VirtualDesktop currentVirtualDesktop;
+        public VirtualDesktop GetCurrentVirtualDesktop()
+        {
+            return currentVirtualDesktop = VirtualDesktop.FromHwnd(GetForegroundWindow());
+        }
+
+        public bool MoveWindowToVD(int desktopIdx)
+        {
+            var targetDesktop = desktops.ElementAt(desktopIdx - 1);
+            VirtualDesktopHelper.MoveToDesktop(GetForegroundWindow(), targetDesktop);
+            return true;
+            
+        }
+
+        public bool ChangeToVD(int desktopIdx)
+        {
+            if (desktopIdx > desktops.Count)
+            {
+
+            }
+            else
+            {
+                var targetDesktop = desktops.ElementAt(desktopIdx - 1);
+
+                targetDesktop.Switch();
+            }
+            return true;
+        }
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetForegroundWindow();
+    }
+}
